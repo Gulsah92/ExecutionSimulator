74d7887a491aab04ecc2b16ca35815994dbf09a5
semih <semihsolmaz@hotmail.com>
2020-05-02 22:35:48 +0300

    started to load, store and print instructions

--- Executor.py
+++ Executor.py
@@ -3,18 +3,19 @@ import CPU
 import sys
 import BinReader
 
-program_name = sys.argv[0]
-file_path = sys.argv[1]
+# program_name = sys.argv[0]
+# file_path = sys.argv[1]
 
 # Instantiate CPU and Memory
 cpu = CPU.cpu()
 mem = Memory.Memory()
 
 # Open binary file and get binaries as list
-binaries = BinReader.Binary(file_path).binl
+binaries = BinReader.Binary('test.bin').binl
 
 # Register dictionary to look up char values of registers
-
+regs = {'0000000000000001' : 'a', '0000000000000010' : 'b', '0000000000000011' : 'c', '0000000000000100' : 'd',
+        '0000000000000101' : 'e', '0000000000000110' : 's', '0000000000000000' : 'pc'}
 
 #Load instructions and data to memory
 for inst in binaries:
@@ -30,16 +31,34 @@ while IsRunning:
     elif mem.get(cpu.pc)[0] == '000010':
         # immediate addressing
         if mem.get(cpu.pc)[1] == '00':
-            cpu.A = mem.get(cpu.pc)[2]
+            # print(mem.get(cpu.pc)[2])
+            cpu.a = mem.get(cpu.pc)[2]
+            # print(cpu)
             cpu.pc = cpu.pc + 1
+            # print(cpu)
         # Todo: Add register, direct memory and indirect memory addressing modes
     # Store op
     elif mem.get(cpu.pc)[0] == '000011':
         # Register addressing
         if mem.get(cpu.pc)[1] == '01':
-            cpu.set()
+            cpu.set(regs[mem.get(cpu.pc)[2]], cpu.get('a'))
             cpu.pc = cpu.pc + 1
-
+            # print(cpu)
+        # Todo: Add direct memory and indirect memory addressing
+    #  Print op
+    elif mem.get(cpu.pc)[0] == '100100':
+        # Register addressing
+        if mem.get(cpu.pc)[1] == '01':
+            bin_asc = cpu.get(regs[mem.get(cpu.pc)[2]])
+            if bin_asc[0:8] == '00000000':
+                dec_asc = int(bin_asc[8:], 2)
+                print(chr(dec_asc))
+                cpu.pc = cpu.pc + 1
+            else:
+                dec_asc_2 = int(bin_asc[8:], 2)
+                dec_asc_1 = int(bin_asc[:8], 2)
+                print(chr(dec_asc_1) + chr(dec_asc_2))
+                cpu.pc = cpu.pc + 1
 
         # {'HALT': '000001', 'LOAD': '000010', 'STORE': '000011', 'ADD': '000100', 'SUB': '000101', 'INC': '000110',
         #  'DEC': '000110', 'MUL': '001000', 'DIV': '001001', 'XOR': '001010', 'AND': '001011', 'OR': '001100',
@@ -48,7 +67,7 @@ while IsRunning:
         #  'JC': '010111', 'JNC': '011000', 'JA': '011001', 'JAE': '100000', 'JB': '100001', 'JBE': '100010',
         #  'READ': '100011', 'PRINT': '100100'}
 
-print(binaries)
-print(str(mem)[0:100])
-
-print(mem.get(cpu.pc))
+# print(binaries)
+# print(str(mem)[0:100])
+#
+# print(mem.get(cpu.pc))
